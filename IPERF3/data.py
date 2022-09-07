e958544ec9efa863f58f2480ab14540b33b6fd5c
Pseth3 <prateeksethi814@gmail.com>
2022-09-07 16:55:39 -0400

    started developing iperf script and input throughput data

---
+++ IPERF3/data.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import os
+
+df = pd.DataFrame(data = {"slice_1":[425,910,500,10],
+                    "slice_2":[900,220,600,80],
+                    "slice_3":[0,0,100,300],
+                    })
+
+df.to_csv(os.path.join(os.getcwd(),"throughput.csv"),index=False)
+df1 = pd.read_csv(os.path.join(os.getcwd(),"throughput.csv"),header=0)
+print(df1)
